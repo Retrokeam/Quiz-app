910a44d73b6f74aac81744d207a629c510442a71
Retrokeam <jrdnsretro@gmail.com>
2023-05-26 10:32:01 +0300

    FIrst commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:adv_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+void main () {
+  runApp(
+     MaterialApp( 
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 86, 15, 209),
+                Color.fromARGB(255, 70, 36, 129),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+          ),
+    ),
+  ),
+);
+}
+
---
+++ lib/start_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget{
+  const StartScreen ({super.key});
+
+  @override
+
+  Widget build(context){
+    return  Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+             Image.asset(
+              'assets/quiz-logo.png', 
+            width: 300,
+            color: Color.fromARGB(150, 248, 245, 245),
+            ),
+          // Opacity(
+          //   opacity: 0.6,
+          //   child: Image.asset(
+          //     'assets/quiz-logo.png', 
+          //   width: 300,),
+          // ),
+          const SizedBox(height: 80,),
+          const Text(
+            'Learn flutter the fun way!',
+            style: TextStyle(
+              color: Color.fromARGB(255, 207, 179, 179),
+              fontSize: 24,
+            ),
+            ),
+            const SizedBox(height: 30,),
+            OutlinedButton.icon(onPressed: (){}, 
+            style: OutlinedButton.styleFrom(
+              foregroundColor: const Color.fromARGB(150, 247, 240, 240),
+            ),
+            icon: Icon(Icons.arrow_right_alt),
+           label: const Text('Start Quiz'),
+           ),
+
+        ],
+      ),);
+  }
+}
